9f1af64adb7aafe3269c9cd678e1d14aaa640a11
Kyuho Son <kyuho.son@samsung.com>
2026-06-19 00:31:52 +0000

    test/unit/bdev: add SLM bdev unit tests
    Add unit-test coverage for the SLM bdev backend.
    
    Signed-off-by: Kyuho Son <kyuho.son@samsung.com>

---
+++ test/unit/lib/bdev/slm/slm_ut.c
@@ -0,0 +1,512 @@
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright (C) 2024 CPCS Implementation Team. All rights reserved.
+ */
+
+#include "spdk_internal/cunit.h"
+#include "spdk_internal/mock.h"
+
+#include "common/lib/ut_multithread.c"
+#include "unit/lib/json_mock.c"
+
+/* Include the actual implementation */
+#include "bdev/slm/vbdev_slm.c"
+#include "bdev/slm/vbdev_pslm.c"
+
+/* Mock implementations */
+DEFINE_STUB_V(spdk_bdev_module_examine_done, (struct spdk_bdev_module *module));
+DEFINE_STUB_V(spdk_bdev_module_list_add, (struct spdk_bdev_module *bdev_module));
+DEFINE_STUB(spdk_bdev_register, int, (struct spdk_bdev *bdev), 0);
+DEFINE_STUB_V(spdk_bdev_io_complete, (struct spdk_bdev_io *bdev_io,
+				      enum spdk_bdev_io_status status));
+DEFINE_STUB_V(spdk_bdev_io_complete_nvme_status, (struct spdk_bdev_io *bdev_io,
+		uint32_t cdw0, int sct, int sc));
+DEFINE_STUB_V(spdk_bdev_io_get_buf, (struct spdk_bdev_io *bdev_io,
+				     spdk_bdev_io_get_buf_cb cb, uint64_t len));
+DEFINE_STUB_V(spdk_copy_buf_to_iovs, (struct iovec *iovs, int iovcnt,
+				      void *buf, size_t buf_len));
+DEFINE_STUB_V(spdk_copy_iovs_to_buf, (void *buf, size_t buf_len,
+				      struct iovec *iovs, int iovcnt));
+DEFINE_STUB(spdk_bdev_first, struct spdk_bdev *, (void), NULL);
+DEFINE_STUB(spdk_bdev_next, struct spdk_bdev *, (struct spdk_bdev *prev), NULL);
+DEFINE_STUB(spdk_bdev_get_nvme_nsid, uint32_t, (struct spdk_bdev *bdev), 0);
+DEFINE_STUB(spdk_bdev_is_slm, bool, (const struct spdk_bdev *bdev), false);
+DEFINE_STUB(spdk_bdev_get_block_size, uint32_t, (const struct spdk_bdev *bdev), 0);
+
+struct spdk_bdev *
+spdk_bdev_get_by_name(const char *bdev_name)
+{
+	struct spdk_bdev_slm *slm;
+
+	if (!bdev_name) {
+		return NULL;
+	}
+
+	TAILQ_FOREACH(slm, &g_slm_bdevs, link) {
+		if (strcmp(bdev_name, slm->bdev.name) == 0) {
+			return &slm->bdev;
+		}
+	}
+
+	return NULL;
+}
+
+/* Custom mock for spdk_bdev_unregister that calls destruct */
+void
+spdk_bdev_unregister(struct spdk_bdev *bdev,
+		     spdk_bdev_unregister_cb cb_fn, void *cb_arg)
+{
+	if (bdev && bdev->fn_table && bdev->fn_table->destruct) {
+		bdev->fn_table->destruct(bdev->ctxt);
+	}
+	if (cb_fn) {
+		cb_fn(cb_arg, 0);
+	}
+}
+
+static void
+test_slm_create_delete(void)
+{
+	int rc;
+	const char *name = "test_slm";
+	uint32_t nsid = 100;
+	uint64_t size_mb = 64;
+	uint32_t granularity = 4;
+
+	/* Test creation */
+	rc = bdev_slm_create(name, nsid, size_mb, granularity);
+	CU_ASSERT(rc == 0);
+
+	/* Verify bdev exists */
+	struct spdk_bdev *bdev = spdk_bdev_get_by_name(name);
+	CU_ASSERT(bdev != NULL);
+	if (bdev) {
+		CU_ASSERT(bdev->blocklen == granularity);
+		CU_ASSERT(bdev->blockcnt == (size_mb * 1024 * 1024 / granularity));
+		CU_ASSERT(bdev->nsid == nsid);
+		CU_ASSERT(bdev->slm == true);
+	}
+
+	/* Test deletion */
+	bdev_slm_delete(name, NULL, NULL);
+}
+
+static void
+test_slm_invalid_params(void)
+{
+	int rc;
+
+	/* Test NULL name */
+	rc = bdev_slm_create(NULL, 100, 64, 4);
+	CU_ASSERT(rc == -EINVAL);
+
+	/* Test zero size */
+	rc = bdev_slm_create("test", 100, 0, 4);
+	CU_ASSERT(rc == -EINVAL);
+
+	/* Test zero granularity */
+	rc = bdev_slm_create("test", 100, 64, 0);
+	CU_ASSERT(rc == -EINVAL);
+}
+
+static void
+test_slm_buffer_access(void)
+{
+	int rc;
+	void *ptr = NULL;
+	const char *name = "test_slm_buf";
+	uint32_t nsid = 200;
+	uint64_t size_mb = 16;
+	uint32_t granularity = 4;
+
+	/* Create SLM bdev */
+	rc = bdev_slm_create(name, nsid, size_mb, granularity);
+	CU_ASSERT(rc == 0);
+
+	/* Test valid buffer access */
+	rc = bdev_slm_get_buffer_ptr(name, 0, 1024, &ptr);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(ptr != NULL);
+
+	/* Test out of bounds access */
+	rc = bdev_slm_get_buffer_ptr(name, 0, 17 * 1024 * 1024, &ptr);
+	CU_ASSERT(rc == -EINVAL);
+
+	/* Test non-existent bdev */
+	rc = bdev_slm_get_buffer_ptr("nonexistent", 0, 1024, &ptr);
+	CU_ASSERT(rc == -ENOENT);
+
+	/* Test NULL pointer */
+	rc = bdev_slm_get_buffer_ptr(name, 0, 1024, NULL);
+	CU_ASSERT(rc == -EINVAL);
+
+	/* Cleanup */
+	bdev_slm_delete(name, NULL, NULL);
+}
+
+static void
+test_slm_multiple_instances(void)
+{
+	int rc;
+	const char *name1 = "slm1";
+	const char *name2 = "slm2";
+	const char *name3 = "slm3";
+
+	/* Create multiple SLM bdevs */
+	rc = bdev_slm_create(name1, 101, 32, 4);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_create(name2, 102, 64, 4);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_create(name3, 103, 128, 4);
+	CU_ASSERT(rc == 0);
+
+	/* Verify all exist */
+	CU_ASSERT(spdk_bdev_get_by_name(name1) != NULL);
+	CU_ASSERT(spdk_bdev_get_by_name(name2) != NULL);
+	CU_ASSERT(spdk_bdev_get_by_name(name3) != NULL);
+
+	/* Delete all */
+	bdev_slm_delete(name1, NULL, NULL);
+	bdev_slm_delete(name2, NULL, NULL);
+	bdev_slm_delete(name3, NULL, NULL);
+}
+
+static void
+test_slm_bdev_access(void)
+{
+	struct spdk_bdev *bdev;
+	struct spdk_bdev fake_bdev = {};
+	void *ptr = (void *)0x1;
+	uint8_t write_buf[16];
+	uint8_t read_buf[16];
+	int rc;
+
+	rc = bdev_slm_create("test_slm_bdev", 301, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	bdev = spdk_bdev_get_by_name("test_slm_bdev");
+	CU_ASSERT_PTR_NOT_NULL_FATAL(bdev);
+
+	memset(write_buf, 0xA5, sizeof(write_buf));
+	memset(read_buf, 0x00, sizeof(read_buf));
+
+	rc = bdev_slm_write_by_bdev(bdev, 128, sizeof(write_buf), write_buf);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_read_by_bdev(bdev, 128, sizeof(read_buf), read_buf);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(memcmp(read_buf, write_buf, sizeof(write_buf)) == 0);
+
+	rc = bdev_slm_get_buffer_ptr_by_bdev(bdev, 128, sizeof(write_buf), &ptr);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT_PTR_NOT_NULL(ptr);
+	CU_ASSERT(memcmp(ptr, write_buf, sizeof(write_buf)) == 0);
+
+	rc = bdev_slm_read_by_bdev(bdev, 0, 0, NULL);
+	CU_ASSERT(rc == 0);
+
+	ptr = (void *)0x1;
+	rc = bdev_slm_get_buffer_ptr_by_bdev(bdev, 0, 0, &ptr);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(ptr == NULL);
+
+	rc = bdev_slm_read_by_bdev(NULL, 0, 1, read_buf);
+	CU_ASSERT(rc == -EINVAL);
+
+	rc = bdev_slm_write_by_bdev(NULL, 0, 1, write_buf);
+	CU_ASSERT(rc == -EINVAL);
+
+	rc = bdev_slm_get_buffer_ptr_by_bdev(NULL, 0, 1, &ptr);
+	CU_ASSERT(rc == -EINVAL);
+
+	rc = bdev_slm_read_by_bdev(&fake_bdev, 0, 1, read_buf);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	rc = bdev_slm_write_by_bdev(&fake_bdev, 0, 1, write_buf);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	rc = bdev_slm_get_buffer_ptr_by_bdev(&fake_bdev, 0, 1, &ptr);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	bdev_slm_delete("test_slm_bdev", NULL, NULL);
+}
+
+static void
+test_slm_bdev_lease_access(void)
+{
+	struct spdk_bdev *bdev;
+	struct spdk_bdev fake_bdev = {};
+	int rc;
+
+	rc = bdev_slm_create("test_slm_lease", 302, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	bdev = spdk_bdev_get_by_name("test_slm_lease");
+	CU_ASSERT_PTR_NOT_NULL_FATAL(bdev);
+
+	rc = bdev_slm_lease_acquire_by_bdev(1, bdev, 0, 64);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_lease_acquire_by_bdev(1, bdev, 0, 64);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_lease_acquire_by_bdev(2, bdev, 32, 64);
+	CU_ASSERT(rc == -EAGAIN);
+
+	rc = bdev_slm_lease_acquire_by_bdev(2, bdev, 128, 64);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_lease_release(1);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_lease_release(2);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_lease_release(3);
+	CU_ASSERT(rc == -ENOENT);
+
+	rc = bdev_slm_lease_acquire_by_bdev(4, &fake_bdev, 0, 64);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	bdev_slm_delete("test_slm_lease", NULL, NULL);
+}
+
+static void
+test_slm_bdev_pin_unpin_api_semantics(void)
+{
+	struct spdk_bdev *bdev;
+	struct spdk_bdev fake_bdev = {};
+	struct spdk_bdev_slm_sg_entry entries[2] = {};
+	uint32_t entry_count;
+	int rc;
+
+	rc = bdev_slm_create("test_slm_pin_api", 307, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	bdev = spdk_bdev_get_by_name("test_slm_pin_api");
+	CU_ASSERT_PTR_NOT_NULL_FATAL(bdev);
+
+	entry_count = UINT32_MAX;
+	rc = bdev_slm_pin_range_by_bdev(bdev, 0, 0, false, entries, SPDK_COUNTOF(entries),
+					&entry_count);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(entry_count == 0);
+
+	entry_count = 0;
+	rc = bdev_slm_pin_range_by_bdev(NULL, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					&entry_count);
+	CU_ASSERT(rc == -EINVAL);
+
+	entry_count = 0;
+	rc = bdev_slm_pin_range_by_bdev(bdev, 0, 1, false, entries, 0, &entry_count);
+	CU_ASSERT(rc == -EINVAL);
+
+	entry_count = 0;
+	rc = bdev_slm_pin_range_by_bdev(bdev, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					&entry_count);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	entry_count = UINT32_MAX;
+	rc = bdev_slm_try_pin_range_by_bdev(bdev, 0, 0, false, entries, SPDK_COUNTOF(entries),
+					    &entry_count);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(entry_count == 0);
+
+	entry_count = 0;
+	rc = bdev_slm_try_pin_range_by_bdev(NULL, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					    &entry_count);
+	CU_ASSERT(rc == -EINVAL);
+
+	entry_count = 0;
+	rc = bdev_slm_try_pin_range_by_bdev(bdev, 0, 1, false, entries, 0, &entry_count);
+	CU_ASSERT(rc == -EINVAL);
+
+	entry_count = 0;
+	rc = bdev_slm_try_pin_range_by_bdev(bdev, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					    &entry_count);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	entry_count = 1;
+	rc = bdev_slm_pin_range_by_bdev(&fake_bdev, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					&entry_count);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	entry_count = 1;
+	rc = bdev_slm_try_pin_range_by_bdev(&fake_bdev, 0, 1, false, entries, SPDK_COUNTOF(entries),
+					    &entry_count);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	rc = bdev_slm_unpin_range_by_bdev(bdev, NULL, 0, false);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_unpin_range_by_bdev(NULL, entries, 1, false);
+	CU_ASSERT(rc == -EINVAL);
+
+	rc = bdev_slm_unpin_range_by_bdev(bdev, entries, 1, false);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	rc = bdev_slm_unpin_range_by_bdev(&fake_bdev, entries, 1, false);
+	CU_ASSERT(rc == -ENOTSUP);
+
+	bdev_slm_delete("test_slm_pin_api", NULL, NULL);
+}
+
+static void
+test_slm_bdev_copy_access(void)
+{
+	struct spdk_bdev *src_bdev;
+	struct spdk_bdev *dst_bdev;
+	struct spdk_bdev fake_bdev = {};
+	uint8_t write_buf[64];
+	uint8_t read_buf[64];
+	int rc;
+
+	rc = bdev_slm_create("test_slm_copy_src", 303, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_create("test_slm_copy_dst", 304, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	src_bdev = spdk_bdev_get_by_name("test_slm_copy_src");
+	dst_bdev = spdk_bdev_get_by_name("test_slm_copy_dst");
+	CU_ASSERT_PTR_NOT_NULL_FATAL(src_bdev);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(dst_bdev);
+
+	memset(write_buf, 0x5C, sizeof(write_buf));
+	memset(read_buf, 0x00, sizeof(read_buf));
+
+	rc = bdev_slm_write_by_bdev(src_bdev, 256, sizeof(write_buf), write_buf);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_copy_by_bdev(dst_bdev, 512, src_bdev, 256, sizeof(write_buf));
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_read_by_bdev(dst_bdev, 512, sizeof(read_buf), read_buf);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(memcmp(write_buf, read_buf, sizeof(write_buf)) == 0);
+
+	rc = bdev_slm_copy_by_bdev(dst_bdev, 0, src_bdev, 0, 0);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_copy_by_bdev(&fake_bdev, 0, src_bdev, 0, sizeof(write_buf));
+	CU_ASSERT(rc == -ENOTSUP);
+
+	rc = bdev_slm_copy_by_bdev(dst_bdev, 0, &fake_bdev, 0, sizeof(write_buf));
+	CU_ASSERT(rc == -ENOTSUP);
+
+	bdev_slm_delete("test_slm_copy_dst", NULL, NULL);
+	bdev_slm_delete("test_slm_copy_src", NULL, NULL);
+}
+
+struct slm_async_result {
+	bool done;
+	int status;
+};
+
+static void
+slm_async_done(void *cb_arg, int status)
+{
+	struct slm_async_result *res = cb_arg;
+
+	res->done = true;
+	res->status = status;
+}
+
+static void
+test_slm_bdev_async_access(void)
+{
+	struct spdk_bdev *src_bdev;
+	struct spdk_bdev *dst_bdev;
+	struct slm_async_result res = {};
+	uint8_t write_buf[64];
+	uint8_t read_buf[64];
+	int rc;
+
+	rc = bdev_slm_create("test_slm_async_src", 305, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	rc = bdev_slm_create("test_slm_async_dst", 306, 8, 4);
+	CU_ASSERT(rc == 0);
+
+	src_bdev = spdk_bdev_get_by_name("test_slm_async_src");
+	dst_bdev = spdk_bdev_get_by_name("test_slm_async_dst");
+	CU_ASSERT_PTR_NOT_NULL_FATAL(src_bdev);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(dst_bdev);
+
+	memset(write_buf, 0xA7, sizeof(write_buf));
+	memset(read_buf, 0x00, sizeof(read_buf));
+
+	res.done = false;
+	res.status = INT32_MIN;
+	rc = bdev_slm_write_by_bdev_async(src_bdev, 0, sizeof(write_buf), write_buf,
+					  slm_async_done, &res);
+	CU_ASSERT(rc == 0);
+	while (!res.done) {
+		poll_threads();
+	}
+	CU_ASSERT(res.status == 0);
+
+	res.done = false;
+	res.status = INT32_MIN;
+	rc = bdev_slm_copy_by_bdev_async(dst_bdev, 128, src_bdev, 0, sizeof(write_buf),
+					 slm_async_done, &res);
+	CU_ASSERT(rc == 0);
+	while (!res.done) {
+		poll_threads();
+	}
+	CU_ASSERT(res.status == 0);
+
+	res.done = false;
+	res.status = INT32_MIN;
+	rc = bdev_slm_read_by_bdev_async(dst_bdev, 128, sizeof(read_buf), read_buf,
+					 slm_async_done, &res);
+	CU_ASSERT(rc == 0);
+	while (!res.done) {
+		poll_threads();
+	}
+	CU_ASSERT(res.status == 0);
+	CU_ASSERT(memcmp(write_buf, read_buf, sizeof(write_buf)) == 0);
+
+	bdev_slm_delete("test_slm_async_dst", NULL, NULL);
+	bdev_slm_delete("test_slm_async_src", NULL, NULL);
+}
+
+int
+main(int argc, char **argv)
+{
+	CU_pSuite suite = NULL;
+	unsigned int num_failures;
+	int rc;
+
+	CU_initialize_registry();
+
+	suite = CU_add_suite("slm", NULL, NULL);
+	CU_ADD_TEST(suite, test_slm_create_delete);
+	CU_ADD_TEST(suite, test_slm_invalid_params);
+	CU_ADD_TEST(suite, test_slm_buffer_access);
+	CU_ADD_TEST(suite, test_slm_multiple_instances);
+	CU_ADD_TEST(suite, test_slm_bdev_access);
+	CU_ADD_TEST(suite, test_slm_bdev_copy_access);
+	CU_ADD_TEST(suite, test_slm_bdev_async_access);
+	CU_ADD_TEST(suite, test_slm_bdev_lease_access);
+	CU_ADD_TEST(suite, test_slm_bdev_pin_unpin_api_semantics);
+
+	allocate_threads(1);
+	set_thread(INVALID_THREAD);
+
+	rc = vbdev_pslm_initialize();
+	if (rc != 0) {
+		CU_cleanup_registry();
+		free_threads();
+		return 1;
+	}
+
+	num_failures = spdk_ut_run_tests(argc, argv, NULL);
+	vbdev_pslm_finish();
+	CU_cleanup_registry();
+
+	free_threads();
+
+	return num_failures;
+}
